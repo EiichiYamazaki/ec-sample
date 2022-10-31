b98683f5b7171bc98ecbb30e2482d3bcd4aea4f4
EciihiYamazaki <eiichi.yamazaki@a1yama.com>
2022-10-31 17:00:42 +0900

    fix: auth

--- src/routes/admin/web.php
+++ src/routes/admin/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('guest:admins')->group(function () {
     Route::get('/', function () {
         return view('admin.welcome');
     });
--- src/routes/front/auth.php
+++ src/routes/front/auth.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Front;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('guest:users')->group(function () {
     Route::get('register', [Front\Auth\RegisteredUserController::class, 'create'])
                 ->name('register');
 
